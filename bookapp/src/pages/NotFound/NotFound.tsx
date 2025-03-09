fe21bfc8dc6bc1d620dcbd8b2afc8633f932488f
SMM-JUJU <s.mohammad.m04@gmail.com>
2025-03-08 17:29:17 -0800

    Page NotFound Added | SMM

--- bookapp/src/pages/NotFound/NotFound.tsx
+++ bookapp/src/pages/NotFound/NotFound.tsx
@@ -1,13 +1,13 @@
 import styles from "./NotFound.module.scss";
 import ghostImage from "./notfound.svg";
+import Footer from "../../components/footer";
 
-const NotFound = () => {
+export default function NotFound() {
   return (
     <div className={styles.notFound}>
       <img src={ghostImage} alt="404 Not Found" />
       <p>صفحه‌ای که دنبالش می‌گردی گم شده!</p>
+      <Footer />
     </div>
   );
-};
-
-export default NotFound;
+}
