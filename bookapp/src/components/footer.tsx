7db1b24f402ea47708248884ac049e00ae288118
SMM-JUJU <s.mohammad.m04@gmail.com>
2025-03-08 17:30:25 -0800

    footer Added | SMM

---
+++ bookapp/src/components/footer.tsx
@@ -0,0 +1,40 @@
+import styles from "./footer.module.scss";
+// import logo from "../logo.svg";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div className={styles.section}>
+          <h3>شرکت</h3>
+          <ul>
+            <li>درباره ما</li>
+            <li>قوانین و مقررات</li>
+          </ul>
+        </div>
+        <div className={styles.section}>
+          <h3>راهنما</h3>
+          <ul>
+            <li>سوالات متداول</li>
+            <li>ایمیل پشتیبانی</li>
+          </ul>
+        </div>
+        <div className={styles.section}>
+          <h3>ما را دنبال کنید</h3>
+          <ul className={styles.section}>
+            <li>اینستاگرام</li>
+            <li>لینکدین</li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.section}>
+        <ul>
+          <br />
+          <br />
+          <br />
+          <li>© ۱۴۰۴ تمامی حقوق محفوظ است.</li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
