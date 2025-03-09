5ef56868c56b9fd8278517100a9f394885c7208d
SMM-JUJU <s.mohammad.m04@gmail.com>
2025-03-09 00:02:29 -0800

    footer udpadet | SMM

--- bookapp/src/components/footer.tsx
+++ bookapp/src/components/footer.tsx
@@ -3,38 +3,36 @@ import styles from "./footer.module.scss";
 
 export default function Footer() {
   return (
-    <footer className={styles.footer}>
-      <div className={styles.container}>
+      <footer className={styles.footer}>
+        <div className={styles.container}>
+          <div className={styles.section}>
+            <h3>شرکت</h3>
+            <ul>
+              <li>درباره ما</li>
+              <li>قوانین و مقررات</li>
+            </ul>
+          </div>
+          <div className={styles.section}>
+            <h3>راهنما</h3>
+            <ul>
+              <li>سوالات متداول</li>
+              <li>ایمیل پشتیبانی</li>
+            </ul>
+          </div>
+          <div className={styles.section}>
+            <h3>ما را دنبال کنید</h3>
+            <ul className={styles.section}>
+              <li>اینستاگرام</li>
+              <li>لینکدین</li>
+            </ul>
+          </div>
+        </div>
         <div className={styles.section}>
-          <h3>شرکت</h3>
           <ul>
-            <li>درباره ما</li>
-            <li>قوانین و مقررات</li>
+            <br />
+            <li>© ۱۴۰۴ تمامی حقوق محفوظ است.</li>
           </ul>
         </div>
-        <div className={styles.section}>
-          <h3>راهنما</h3>
-          <ul>
-            <li>سوالات متداول</li>
-            <li>ایمیل پشتیبانی</li>
-          </ul>
-        </div>
-        <div className={styles.section}>
-          <h3>ما را دنبال کنید</h3>
-          <ul className={styles.section}>
-            <li>اینستاگرام</li>
-            <li>لینکدین</li>
-          </ul>
-        </div>
-      </div>
-      <div className={styles.section}>
-        <ul>
-          <br />
-          <br />
-          <br />
-          <li>© ۱۴۰۴ تمامی حقوق محفوظ است.</li>
-        </ul>
-      </div>
-    </footer>
+      </footer>
   );
 }
