76b7f5fe8d4e879aaeead409a6679104bd4d3b6e
SMM-JUJU <s.mohammad.m04@gmail.com>
2025-03-08 18:27:04 -0800

    App.tsx updated to show AboutUs page | SMM

--- bookapp/src/App.tsx
+++ bookapp/src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css';
+import "./App.css";
 // import '/index.css';
-import SignUp from './pages/SignUp/Signup';
-import NotFound from './pages/NotFound/NotFound';
-
+import SignUp from "./pages/SignUp/Signup";
+import NotFound from "./pages/NotFound/NotFound";
+import AboutUs from "./pages/AboutUs/AboutUs";
 function App() {
   return (
     <Router>
@@ -12,8 +12,8 @@ function App() {
         <Route path="/" element={<h1>Home Page</h1>} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/notfound" element={<NotFound />} />
-        </Routes>
-
+        <Route path="/aboutus" element={<AboutUs />} />
+      </Routes>
     </Router>
   );
 }
