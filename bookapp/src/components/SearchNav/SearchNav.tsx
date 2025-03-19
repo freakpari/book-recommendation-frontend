dadb83bcd21a6c3fe572f38fd04f5e683e897378
SMM-JUJU <s.mohammad.m04@gmail.com>
2025-03-18 22:04:24 -0700

    search navigation component updated | SMM

--- bookapp/src/components/SearchNav/SearchNav.tsx
+++ bookapp/src/components/SearchNav/SearchNav.tsx
@@ -1,30 +1,24 @@
 import React from "react";
 import styles from "./SearchNav.module.scss";
 import menu from "./icons/menu.svg"
-import user from "../SideProfile/icons/AliMohamadi.svg"
+import user from "./icons/AliMohamadi.svg"
 import logo from "./icons/logo.svg"
 import searchIcon from "./icons/searchButton.svg"
 
-export default function searchNav (){
+export default function SearchNav (){
     return (
         <div className={styles.container}>
 
-            <div className={styles.menu}>
-                <img src={menu} alt="menu" />
-            </div>
+            <img className={styles.menu} src={menu} alt="menu" />
 
-            <div className={styles.userIcon}>
-                <img src={user} alt="user icon" />
-            </div>
+            <img className={styles.userIcon} src={user} alt="user icon" />
 
             <div className={styles.searchBar}>
                 <input type="search" placeholder="جستجو" />
                 <img src={searchIcon} alt="search button" />
             </div>
 
-            <div className={styles.logoIcon}>
-                <img  src={logo} alt="logo icon"/>
-            </div>
+            <img className={styles.logoIcon} src={logo} alt="logo icon"/>
 
         </div>
     )
