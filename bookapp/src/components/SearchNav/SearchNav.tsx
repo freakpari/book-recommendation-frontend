f359a9a2de346ad9c48adfc13d44e3d0c1172753
freakpari <parivashcheraghi40@gmail.com>
2025-03-28 14:32:33 +0330

    searchnav updated | parivash cheraghi

--- bookapp/src/components/SearchNav/SearchNav.tsx
+++ bookapp/src/components/SearchNav/SearchNav.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import styles from "./SearchNav.module.scss";
 import menu from "./icons/menu.svg"
-import user from "../SideProfile/icons/AliMohamadi.svg"
+import user from "./icons/AliMohamadi.svg"
 import logo from "./icons/logo.svg"
-// import searchIcon from "./icons/searchButton.svg"
+import searchIcon from "./icons/searchButton.svg"
 
 export default function SearchNav (){
     return (
@@ -11,13 +11,11 @@ export default function SearchNav (){
 
             <img className={styles.menu} src={menu} alt="menu" />
 
-            <div className={styles.userIcon}>
-                <img src={user} alt="user icon" />
-            </div>
+            <img className={styles.userIcon} src={user} alt="user icon" />
 
             <div className={styles.searchBar}>
                 <input type="search" placeholder="جستجو" />
-                {/* <img src={searchIcon} alt="search button" /> */}
+                <img  src={searchIcon} alt="search button" />
             </div>
 
             <img className={styles.logoIcon} src={logo} alt="logo icon"/>
