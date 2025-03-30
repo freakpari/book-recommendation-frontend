5b02f55c40baffca22116b0f9dbfd701a2a64600
SMM-JUJU <s.mohammad.m04@gmail.com>
2025-03-30 02:37:08 -0700

    EditProfile page updated | SMM

--- bookapp/src/pages/EditProfile/EditProfile.tsx
+++ bookapp/src/pages/EditProfile/EditProfile.tsx
@@ -1,97 +1,220 @@
 import React from "react";
+import {useState} from "react";
 import styles from "./EditProfile.module.scss";
-import Footer from "../../components/footer/footer"
-import SideProfile from "../../components/SideProfile/SideProfile"
-import SearchNav from "../../components/SearchNav/SearchNav"
+import Footer from "../../components/Footer/Footer";
+import SideProfile from "../../components/SideProfile/SideProfile";
+import SearchNav from "../../components/SearchNav/SearchNav";
+import { motion, AnimatePresence } from "framer-motion";
+import {ChevronDown} from "lucide-react";
+import { Eye, EyeOff } from "lucide-react";
+
 
 export default function EditProfile() {
+
+    const [daySelectedValue, setDaySelectedValue] = React.useState("");
+    const dayStartValue = 1;
+
+    const [yearSelectedValue, setYearSelectedValue] = React.useState("");
+    const yearStartValue = 1320;
+
+    const [monthSelectedValue, setMonthSelectedValue] = React.useState("");
+    const months = [
+        { id: 1, name: "فروردین" },
+        { id: 2, name: "اردیبهشت" },
+        { id: 3, name: "خرداد" },
+        { id: 4, name: "تیر" },
+        { id: 5, name: "مرداد" },
+        { id: 6, name: "شهریور" },
+        { id: 7, name: "مهر" },
+        { id: 8, name: "آبان" },
+        { id: 9, name: "آذر" },
+        { id: 10, name: "دی" },
+        { id: 11, name: "بهمن" },
+        { id: 12, name: "اسفند" },
+    ];
+
+    const [selectedGender, setSelectedGender] = useState<"زن" | "مرد" | "ترجیح می‌دهم نگویم" |null>(null);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const [showPassword, setShowPassword] = useState(false);
+
     return (
-        <div className={styles.container}>
-            <SearchNav />
+            <div className={styles.container}>
+                <SearchNav/>
 
-            <div className={styles.update}>
+                <div className={styles.update}>
 
-                <form action="">
+                    <form action="">
 
-                    <input className={styles.userName} type="text"/>
-                    <input className={styles.userEmail} type="email"/>
-                    {/*<br/>*/}
-                    <input className={styles.fName} type="text"/>
-                    <label htmlFor="day"></label>
-                    <select className={styles.dayOfBirth} id="day" name="day">
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
-                        <option value="13">13</option>
-                        <option value="14" selected={true}>14</option>
-                        <option value="15">15</option>
-                        <option value="16">16</option>
-                        <option value="17">17</option>
-                        <option value="18">18</option>
-                        <option value="19">19</option>
-                        <option value="20">20</option>
-                        <option value="21">21</option>
-                        <option value="22">22</option>
-                        <option value="23">23</option>
-                        <option value="24">24</option>
-                        <option value="25">25</option>
-                        <option value="26">26</option>
-                        <option value="27">27</option>
-                        <option value="28">28</option>
-                        <option value="29">29</option>
-                        <option value="30">30</option>
-                    </select>
-                    <label htmlFor="month"></label>
-                    <select className={styles.monthOfBirth} id="month" name="month">
-                        <option value="1">فروردین</option>
-                        <option value="2">اردیبهشت</option>
-                        <option value="3">خرداد</option>
-                        <option value="4">تیر</option>
-                        <option value="5" selected={true}>مرداد</option>
-                        <option value="6">شهریور</option>
-                        <option value="7">مهر</option>
-                        <option value="8">آبان</option>
-                        <option value="9">آذر</option>
-                        <option value="10">دی</option>
-                        <option value="11">بهمن</option>
-                        <option value="12">اسفند</option>
-                    </select>
-                    <label htmlFor="year"></label>
-                    <select className={styles.yearOfBirth} id="year" name="year">
-                        <option value="1383">1383</option>
-                        <option value="1382">1382</option>
-                        <option value="1381">1381</option>
-                    </select>
-                    <br/>
-                    <input type="text"/>
-                    <input type="password"/>
-                    <br/>
-                    <input type="text"/>
-                    <button type="submit">تغییر رمزعبور</button>
-                    <label htmlFor="gender"></label>
-                    <select id="gender" name="gender">
-                        <option value="man">مرد</option>
-                        <option value="woman">زن</option>
-                        <option value="null">جنسیت</option>
-                    </select>
-                </form>
+                        <input className={styles.userName} type="text" name="userName" id="userName"
+                               placeholder="نام کاربری"/>
+                        <input className={styles.userEmail} type="email" name='email' id="email" placeholder="ایمیل"/>
+
+                        <input className={styles.firstName} type="text" name="firstName" id="firstName"
+                               placeholder="نام"/>
+
+                        <select
+                            className={`${styles.dayOfBirth} ${daySelectedValue ? styles.selected : ""}`}
+                            id="dayOfBirth"
+                            name="dayOfBirth"
+                            value={daySelectedValue}
+                            onChange={(e) => setDaySelectedValue(e.target.value)}
+
+                        >
+                            {Array.from({length: 31}, (_, i) => (
+                                <option key={i + dayStartValue} value={i + dayStartValue}>
+                                    {i + dayStartValue}
+                                </option>
+                            ))}
+                        </select>
+                        <select
+                            className={`${styles.monthOfBirth} ${monthSelectedValue ? styles.selected : ""}`}
+                            id="monthOfBirth"
+                            name="monthOfBirth"
+                            value={monthSelectedValue}
+                            onChange={(e) => setMonthSelectedValue(e.target.value)}
+                        >
+                            {months.map((month) => (
+                                <option key={month.id} value={month.id}>
+                                    {month.name}
+                                    <ChevronDown/>
+                                </option>
+                            ))}
+                        </select>
+                        <select
+                            className={`${styles.yearOfBirth} ${yearSelectedValue ? styles.selected : ""}`}
+                            id="yearOfBirth"
+                            name="yearOfBirth"
+                            value={yearSelectedValue}
+                            onChange={(e) => setYearSelectedValue(e.target.value)}
+                        >
+                            {Array.from({length: 81}, (_, i) => (
+                                <option key={i + yearStartValue} value={i + yearStartValue}>
+                                    {i + yearStartValue}
+                                </option>
+                            ))}
+                        </select>
+
+                        <input className={styles.lastName} type="text" name="lastName" id="lastName"
+                               placeholder="نام خانوادگی"/>
+                        <>
+                            <input
+                                className={styles.password}
+                                type={showPassword ? "text" : "password"}
+                                name="password"
+                                id="password"
+                                minLength={8}
+                                placeholder="رمز عبور"
+                            />
+                            <button
+                                type="button"
+                                className={styles.showPasswordBtn}
+                                onClick={() => setShowPassword((prev) => !prev)}
+                            >
+                                {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                            </button>
+                        </>
+
+
+
+
+
+                        <input className={styles.bio} type="text" name="bio" id="bio" maxLength={40}
+                               placeholder="یه چیزی درمورد خودت بنویس!"/>
+                        <button className={styles.changePassBtn}>تغییر رمز عبور</button>
+
+
+                        <div className={styles.dropdown}>
+                            <button
+                                type="button"
+                                className={`${styles.gender} ${selectedGender ? styles.selected : ""}`}
+                                onClick={() => setIsOpen((prev) => !prev)}
+                            >
+                                {selectedGender ? selectedGender : "جنسیت"}
+                                <motion.div
+                                    animate={{ rotate: isOpen ? 180 : 0 }}
+                                    transition={{ duration: 0.3, ease: "easeOut" }}
+                                    className={styles.iconWrapper}
+                                >
+                                    <ChevronDown />
+                                </motion.div>
+                            </button>
+
+                            <AnimatePresence>
+                                {isOpen && (
+                                    <motion.div
+                                        initial={{ opacity: 0, height: 0 }}
+                                        animate={{ opacity: 1, height: "auto" }}
+                                        exit={{ opacity: 0, height: 0 }}
+                                        transition={{ duration: 0.3, ease: "backInOut" }}
+                                    >
+                                        <div className={styles.dropDownMenu}>
+                                            {selectedGender !== "زن" && (
+                                                <>
+                                                    <button
+                                                        type="button"
+                                                        className={styles.option}
+                                                        onClick={() => {
+                                                            setSelectedGender("زن");
+                                                            setIsOpen(false);
+                                                        }}
+                                                    >
+                                                        زن
+                                                    </button>
+                                                    <hr className={styles.hr} />
+                                                </>
+                                            )}
+
+                                            {selectedGender !== "مرد" && (
+                                                <>
+                                                    <button
+                                                        type="button"
+                                                        className={styles.option}
+                                                        onClick={() => {
+                                                            setSelectedGender("مرد");
+                                                            setIsOpen(false);
+                                                        }}
+                                                    >
+                                                        مرد
+                                                    </button>
+                                                    {selectedGender !== "ترجیح می‌دهم نگویم" && <hr className={styles.hr} />}
+                                                </>
+                                            )}
+
+                                            {selectedGender !== "ترجیح می‌دهم نگویم" && (
+                                                <button
+                                                    type="button"
+                                                    className={styles.option}
+                                                    onClick={() => {
+                                                        setSelectedGender("ترجیح می‌دهم نگویم");
+                                                        setIsOpen(false);
+                                                    }}
+                                                >
+                                                    ترجیح می‌دهم نگویم
+                                                </button>
+                                            )}
+                                        </div>
+                                    </motion.div>
+                                )}
+                            </AnimatePresence>
+                        </div>
+
+                        <input className={styles.phoneNumber} type="number" name="phoneNumber" id="phoneNumber"
+                               placeholder="۰۹۱۳۹۸۶۳۰۵۶"/>
+
+
+
+                        <input className={styles.updateProfileBtn} type="submit" name="updateProfile" id="updateProfile"
+                               value="به‌روزرسانی"/>
+                    </form>
+                </div>
+
+                <SideProfile/>
+
+                <div>
+                    <Footer/>
+                </div>
+
             </div>
-
-            <SideProfile/>
-
-            <div>
-                <Footer/>
-            </div>
-
-        </div>
     )
 }
