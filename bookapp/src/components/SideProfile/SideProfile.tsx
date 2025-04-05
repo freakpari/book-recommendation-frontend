241ab20c829112b3db5596426f8bd08effb301bc
SMM-JUJU <s.mohammad.m04@gmail.com>
2025-04-05 12:15:48 -0700

    links to other pages updated & add delete profile button | SMM

--- bookapp/src/components/SideProfile/SideProfile.tsx
+++ bookapp/src/components/SideProfile/SideProfile.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import styles from "./SideProfile.module.scss";
 import pencil from "./icons/Pencil.svg";
 import heart from "./icons/Heart.svg";
@@ -6,10 +6,19 @@ import history from "./icons/History.svg";
 import list from "./icons/list.svg";
 import defaultUser from "./icons/defaultUser.svg";
 import editPen from "./icons/editPen.svg";
+import deleteIcon from "./icons/Trash.svg"
+
 
 export default function SideProfile() {
     const [profileImage, setProfileImage] = useState<string | null>(null);
     const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+    useEffect(() => {
+        const savedImage = localStorage.getItem("profileImage");
+        if (savedImage) {
+            setProfileImage(savedImage);
+        }
+    }, []);
 
     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const file = e.target.files?.[0];
@@ -28,19 +37,33 @@ export default function SideProfile() {
 
         const imageUrl = URL.createObjectURL(file);
         setProfileImage(imageUrl);
+
+        localStorage.setItem("profileImage", imageUrl);
     };
 
+    const handleDeleteImage = () => {
+        setProfileImage(null);
+        localStorage.removeItem("profileImage");
+    };
 
     return (
         <div className={styles.container}>
             <div className={styles.head}>
-                <button onClick={() => fileInputRef.current?.click()}>
+
+                <button className={styles.editBtn} onClick={() => fileInputRef.current?.click()}>
                     <img src={editPen} alt="edit profile"/>
+
                 </button>
+
+                {profileImage && (
+                <button onClick={handleDeleteImage} className={styles.deleteBtn}>
+                    <img src={deleteIcon} alt="delete profile"/>
+                </button>
+                )}
+
                 <input
                     type="file"
                     ref={fileInputRef}
-                    // accept="image/png, image/jpeg, image/webp"
                     onChange={handleFileChange}
                     style={{ display: "none" }}
                 />
@@ -53,24 +76,26 @@ export default function SideProfile() {
 
                 <h2>علی محمدی</h2>
                 <h6>یه عاشق کتاب که مهندسه</h6>
+
+
             </div>
 
             <div className={styles.options}>
-                <a href="http://localhost:3000/editProfile">
+                <a href="/editProfile">
                     <button>
                         <img src={pencil} alt="pencil logo" />
                         <p>ویرایش حساب کاربری</p>
                     </button>
                 </a>
                 <hr className={styles.hr} />
-                <a href="http://localhost:3000/myFavoriteBook">
+                <a href="/myFavoriteBook">
                     <button>
                         <img src={heart} alt="heart logo" />
                         <p>مورد علاقه‌ها</p>
                     </button>
                 </a>
                 <hr className={styles.hr} />
-                <a href="http://localhost:3000/myBookHistory">
+                <a href="/myBookHistory">
                     <button>
                         <img src={history} alt="history logo" />
                         <p>تاریخچه</p>
