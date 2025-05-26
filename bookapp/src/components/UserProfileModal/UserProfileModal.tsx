ad508312f9b9894a88ac54812bf40a3957714805
SMM-JUJU <s.mohammad.m04@gmail.com>
2025-05-26 07:12:28 -0700

    UserProfileModal connected to API | SMM

--- bookapp/src/components/UserProfileModal/UserProfileModal.tsx
+++ bookapp/src/components/UserProfileModal/UserProfileModal.tsx
@@ -1,25 +1,39 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styles from "./UserProfileModal.module.scss";
-import Maryam from "./icons/Maryam.svg"
 import Tehran from "../../pages/MyBookList/icons/Tehran.svg";
-import Koodak from "../../pages/MyBookList/icons/Koodak.svg";
 import {ReactComponent as Send} from "./icons/Send.svg";
 import { ReactComponent as PlusCircle } from  "./icons/PlusCircle.svg";
+import axios from "axios";
+import {AnimatePresence, motion} from "framer-motion";
+import defaultUser from "../SideProfile/icons/defaultUser.svg";
+import { useNotification, NotificationModal } from "../../components/NotificationManager/NotificationManager";
 
 interface Props {
     onClose: () => void;
+    userid: string;
 }
 
-const userList = [
-    { title: "تهرانم", includes: "سری کتاب‌های هری پاتر، سری کتاب‌های ارباب حلقه‌ها، سری کتاب‌های بازی تاج و تخت", image: Tehran },
-    { title: "کودک", includes: "شامل: سری کتاب‌های کودک ۱ ٬ اتل متل توتوله گاو حسن چجوره٬ نه شیر داره نه پستون یک زن هندی بسون", image: Koodak },
-    { title: "تهرانم", includes: "سری کتاب‌های هری پاتر، سری کتاب‌های ارباب حلقه‌ها، سری کتاب‌های بازی تاج و تخت", image: Tehran },
-    { title: "کودک", includes: "شامل: سری کتاب‌های کودک ۱ ٬ اتل متل توتوله گاو حسن چجوره٬ نه شیر داره نه پستون یک زن هندی بسون", image: Koodak },
-    { title: "تهرانم", includes: "سری کتاب‌های هری پاتر، سری کتاب‌های ارباب حلقه‌ها، سری کتاب‌های بازی تاج و تخت", image: Tehran },
-    { title: "کودک", includes: "شامل: سری کتاب‌های کودک ۱ ٬ اتل متل توتوله گاو حسن چجوره٬ نه شیر داره نه پستون یک زن هندی بسون", image: Koodak },
-    { title: "تهرانم", includes: "سری کتاب‌های هری پاتر، سری کتاب‌های ارباب حلقه‌ها، سری کتاب‌های بازی تاج و تخت", image: Tehran },
-    { title: "کودک", includes: "شامل: سری کتاب‌های کودک ۱ ٬ اتل متل توتوله گاو حسن چجوره٬ نه شیر داره نه پستون یک زن هندی بسون", image: Koodak },
-];
+interface UserInformation  {
+    CollectionID: number,
+    IsPublic: boolean,
+    Title: string,
+    CreateDate: string,
+    Discription: string,
+    ReportID: null,
+    GenreID1: number,
+    GenreTitle1: null,
+    GenreID2: number,
+    GenreTitle2: null,
+    GenreID3: number,
+    GenreTitle3: null,
+    AccessibilityGroupID: null,
+    AccessGroupTitle: null,
+    AccessGroupDiscription: null,
+    NumberOfDetail: number,
+    UserID: number,
+    UserName: string,
+    FullName: string,
+}
 
 function BookListCard({ title, includes, image }: { title: string, includes: string, image: string }) {
     return (
@@ -43,9 +57,93 @@ function BookListCard({ title, includes, image }: { title: string, includes: str
     );
 }
 
-export default function UserProfileModal ({ onClose }: Props) {
+export default function UserProfileModal ({ onClose , userid}: Props) {
+
+    const {
+        showNotification,
+        notificationMessage,
+        notificationType,
+        setShowNotification,
+        showNotificationMessage
+    } = useNotification();
+    const [userInfo, setUserInfo] = useState<UserInformation[]>([]);
+    const [userFullName, setUserFullName] = useState('');
+    const [userName, setUserName] = useState('');
+    const [profileImage, setProfileImage] = useState<string | null>(null);
+
+    useEffect(() => {
+
+        const handleUserProfilePicture = async () => {
+            const token = localStorage.getItem("token");
+            if(!token) {
+                console.error("دسترسی غیرمجاز");
+                return;
+            }
+            const useri = "10113";
+            try {
+                const response = await axios.get(`https://intelligent-shockley-8ynjnlm8e.liara.run/api/auth/profilePic/${useri}`,
+                    {
+                        responseType: "blob"
+                    }
+            )
+                const imageBlob = response.data;
+                const imageURL = URL.createObjectURL(imageBlob);
+
+                setProfileImage(imageURL);
+
+            } catch (error: any) {
+                if (error.code === 'ECONNABORTED') {
+                    showNotificationMessage("سرور پاسخ نداد. لطفاً بعداً تلاش کنید.", 'error');
+                }
+                else {
+                    showNotificationMessage("خطایی رخ داد. لطفاً دوباره تلاش کنید.",'error');
+                }
+            }
+
+        };
+
+
+        const handleUserInfo = async () => {
+            const token = localStorage.getItem("token");
+            if(!token) {
+                console.error("دسترسی غیرمجاز");
+                return;
+            }
+
+            const useri = "10115";
+            try {
+                const response = await axios.get<UserInformation[]>(`https://intelligent-shockley-8ynjnlm8e.liara.run/api/collection/anotherUser/${useri}`)
+                setUserInfo(response.data);
+                if(response.data.length > 0) {
+                    setUserFullName(response.data[0]?.FullName);
+                    setUserName(response.data[0]?.UserName);
+                }
+            }
+            catch (error: any) {
+                if (error.code === 'ECONNABORTED') {
+                    showNotificationMessage("سرور پاسخ نداد. لطفاً بعداً تلاش کنید.", 'error');
+                }
+                else {
+                    showNotificationMessage("خطایی رخ داد. لطفاً دوباره تلاش کنید.",'error');
+                }
+            }
+        };
+
+        handleUserInfo();
+        handleUserProfilePicture();
+    }, []);
+
     return (
         <div>
+            <AnimatePresence>
+                {showNotification && (
+                    <NotificationModal
+                        message={notificationMessage}
+                        type={notificationType}
+                        onClose={() => setShowNotification(false)}
+                    />
+                )}
+            </AnimatePresence>
             <div
                 className={styles.overlay}
                 onClick={onClose}
@@ -56,15 +154,17 @@ export default function UserProfileModal ({ onClose }: Props) {
                 <div className={styles.halfBorderCircle}>
                     <div className={styles.circle}>
                         <div className={styles.hideMainBorder}></div>
-                        <img src={Maryam} alt="" />
+                        <img src={profileImage || defaultUser} alt="" />
                     </div>
                 </div>
                 <div className={styles.modalContent}>
                     <div className={styles.profile}>
                         <div className={styles.userInfo}>
-                            <div className={styles.userId}>@marybooklover</div>
+                            <div className={styles.userId}>@{userName}</div>
                             <div className={styles.userNameType}>
-                                <div className={styles.userName}>مریم ساداتی</div>
+                                <div className={styles.userName}>
+                                    {userFullName}
+                                </div>
                                 <div>ENFP</div>
                             </div>
                             <div className={styles.userBio}>مری جون هستم کسی که منریدنیستردنتسیذرنتلذسبنتلرذ</div>
@@ -77,18 +177,21 @@ export default function UserProfileModal ({ onClose }: Props) {
                     <div className={styles.listHeader}>لیست‌های ساخته شده</div>
                     <div className={styles.bookListContainer}>
                         <div className={styles.userListDrawer}>
-                            {/*<div className={styles.noList}>هیچ لیستی توسط این کاربر ساخته نشده است</div>*/}
-
-                            {userList.map((list, index) => (
-                                <BookListCard
-                                    key={`my-list-${index}`}
-                                    title={list.title}
-                                    includes={list.includes}
-                                    image={list.image}
-                                />
-                            ))}
+                            {userInfo.length === 0 ? (
+                                <div className={styles.noList}>هیچ لیستی توسط این کاربر ساخته نشده است</div>
+                            ) : (
+                                userInfo.map((item, index) => (
+                                    <BookListCard
+                                        key={`user-list-${index}`}
+                                        title={item.Title}
+                                        includes={item.Discription || 'بدون توضیح'}
+                                        image={Tehran}
+                                    />
+                                ))
+                            )}
                         </div>
                     </div>
+
                 </div>
             </div>
         </div>
